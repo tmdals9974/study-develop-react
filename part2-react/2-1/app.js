71d018b8c8d2b89455a6933aa0c9e17becc1f040
dev_sm <tmdals9974@naver.com>
2023-01-15 21:32:17 +0900

    feat: 2-1강 학습내용

---
+++ part2-react/2-1/app.js
@@ -0,0 +1,27 @@
+const list = document.querySelector(".list-group");
+const todoContainer = document.createElement("li");
+const todoItem = document.createElement("div");
+const todoStatus = document.createElement("input");
+const todoLabel = document.createElement("label");
+const deleteButton = document.createElement("button");
+
+todoContainer.classList.add("list-group-item", "list-group-flat");
+
+todoContainer.appendChild(todoItem);
+
+todoItem.appendChild(todoStatus);
+todoItem.appendChild(todoLabel);
+todoItem.appendChild(deleteButton);
+
+todoItem.classList.add("checkbox");
+
+todoStatus.setAttribute("id", "checkbox");
+todoStatus.setAttribute("type", "checkbox");
+
+todoLabel.setAttribute("for", "checkbox");
+todoLabel.textContent = "첫 번째 할 일";
+
+deleteButton.classList.add("btn", "btn-outline-secondary");
+deleteButton.textContent = "X";
+
+list.appendChild(todoContainer);
