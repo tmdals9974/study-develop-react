59105c6d3ba542e5d4fb806540d97897cf8b2e13
dev_sm <tmdals9974@naver.com>
2023-01-26 16:33:11 +0900

    feat: 4-4강 학습내용

---
+++ part4-redux_middleware/4-4/redux.js
@@ -0,0 +1,41 @@
+export const actionCreator = (type) => (payload) => ({
+  type,
+  payload,
+});
+
+export function createStore(reducer, middlewares = []) {
+  let state;
+  const handlers = [];
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    handlers.forEach((handler) => handler());
+  }
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(handler) {
+    handlers.push(handler);
+  }
+
+  const store = {
+    getState,
+    subscribe,
+    dispatch,
+  };
+
+  /* middleware start */
+  middlewares = Array.from(middlewares).reverse();
+  let lastDispatch = dispatch;
+
+  middlewares.forEach((middleware) => {
+    lastDispatch = middleware(store)(lastDispatch);
+  });
+
+  store.dispatch = lastDispatch;
+  /* middleware end */
+
+  return store;
+}
